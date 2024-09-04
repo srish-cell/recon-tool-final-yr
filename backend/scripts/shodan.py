4c8fc4c8d2cc229795adc75b5c505b15fed3dc9d
srish-cell <srishnambirajan@gmail.com>
2024-09-04 11:21:11 +0530

    shodan update

--- backend/scripts/shodan.py
+++ backend/scripts/shodan.py
@@ -1,10 +1,9 @@
 import subprocess
 import shodan
 import socket
-API_KEY = 'cNKl3a6MYaHsDVgz4TWmyJVQLja3yiha'
+API_KEY = 'EHZGgmpZzkyCMp5BOg5Ij3AqyZ0VegU5'
 
-api = shodan.Shodan(API_KEY)
-
+cmd=f"shodan init {API_KEY}"
 def resolve_domain(domain):
     try:
         return socket.gethostbyname(domain)
@@ -12,29 +11,15 @@ def resolve_domain(domain):
         print(f"Error: Unable to resolve domain {domain}")
         return None
 
-def lookup_host(ip):
+def lookup_host(ips):
+    
     try:
-        if isinstance(ips, str):  # Handle a single IP as a string
-            ips = [ips]
-        elif not isinstance(ips, (list, tuple)):  # If it's not a list or tuple, raise an error
-            raise ValueError("IP must be a string or a list/tuple of strings")
+        command = f"shodan host {ips}"
+        subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.stdout.decode()
 
-        host=self._request('/shodan/host/{}'.format(','.join(ips)), params)
-
-        print(f"IP: {host['ip_str']}")
-        print(f"Organization: {host.get('org', 'N/A')}")
-        print(f"Operating System: {host.get('os', 'N/A')}")
-
-        for item in host['data']:
-            print(f"\nPort: {item['port']}")
-            print(f"Banner: {item['data']}")
         
-        if 'vulns' in host:
-            print("\nVulnerabilities:")
-            for vuln in host['vulns']:
-                print(f"CVE: {vuln.replace('!', '')}")
-        return host       
-    except shodan.APIError as e:
-        print(f"Error: {e}")
-        return e
+    except subprocess.CalledProcessError as e:
+        return e.stderr.decode()
 
